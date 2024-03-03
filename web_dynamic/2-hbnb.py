4b7390769a53b4f875496b3e2b5212bb5d60f0be
Kwesi20000 <hendrickskumi@yahoo.com>
2024-03-03 15:31:26 -0800

    done

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -17,16 +17,16 @@ host = '0.0.0.0'
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    On after each request, this method calls .close() (i.e. .remove()) on
+    On each request, this method calls .close() (i.e. .remove()) on
     the current SQLAlchemy Session
     """
     storage.close()
 
 
-@app.route('/1-hbnb')
+@app.route('/2-hbnb')
 def hbnb_filters(the_id=None):
     """
-    This handles request to custom template with states, cities & amentities
+    this handles request to custom template with states, cities & amentities
     """
     state_objs = storage.all('State').values()
     states = dict([state.name, state] for state in state_objs)
@@ -34,7 +34,7 @@ def hbnb_filters(the_id=None):
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
-    return render_template('1-hbnb.html',
+    return render_template('2-hbnb.html',
                            cache_id=uuid.uuid4(),
                            states=states,
                            amens=amens,
